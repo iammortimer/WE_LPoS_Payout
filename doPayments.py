c68c75309e80cf92ef5640410112d356ebd3087a
iammortimer <info@mullerbeheer.nl>
2020-05-25 21:39:12 +0200

    masstx fee recalculation

--- doPayments.py
+++ doPayments.py
@@ -28,10 +28,10 @@ def main():
         url = '/transactions/sign'
         print('doing testpayment, no funds are actually transferred...')
 
-    fee = 10000000 + ((len(payments)+1 / 2) * 10000000)
+    fee = 10000000 + (((len(payments) + 1) / 2) * 10000000)
 
-    if fee < 30000000:
-        fee = 30000000
+    # if fee < 30000000:
+    #     fee = 30000000
 
     # if fee > 30000000 and fee < 60000000:
     #     fee = 60000000
